bd7e4642a7b4ab75d198dacf585094ed01ad5138
Andrew Chen <aandrewc@ucla.edu>
2019-10-11 19:55:12 -0700

    filling in get_states and get_actions

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from mdp import MDP
 
 
-def get_states():
-	pass
+def get_states(L, W):
+	S = []
+	for x in range(L):
+	    for y in range(W):
+	        for h in range(12):
+	            S.append((x, y, h))
+	return S
 
 
 def get_actions():
-	pass
+	A = [(0, 0)]
+	for move in [1, -1]:
+	    for rotation in [0, -1, 1]:
+	        A.append((move, rotation))
+    return A
 
 
 def P_function(s, a, s_):
@@ -19,8 +28,9 @@ def R_function(s, a, s_):
 
 gamma = 0.9
 
-
-states = get_states()
+L = 8
+W = 8
+states = get_states(L, W)
 actions = get_actions()
 
 mdp = MDP(states, actions, P_function, R_function, gamma)
