614fd9dcfc03c4c13c414c738a1b380efb4579f6
Meet Taraviya <meet11061997@gmail.com>
2019-10-18 16:49:37 +0530

    Added initial policy, combined code for different answers, wrote test cases

--- main.py
+++ main.py
@@ -1,153 +1,71 @@
-from mdp import MDP
+from mdp import *
 import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
+from random import choice
 
-def get_states(L, W):
-	S = []
-	for x in range(L):
-	    for y in range(W):
-	        for h in range(12):
-	            S.append((x, y, h))
-	return S
+def get_states():
+    S = []
+    for x in range(L):
+        for y in range(W):
+            for h in range(12):
+                S.append((x, y, h))
+    return S
 
 
 def get_actions():
-	A = [(0, 0)]
-	for move in [1, -1]:
-	    for rotation in [0, -1, 1]:
-	        A.append((move, rotation))
-	return A
+    A = [(0, 0)]
+    for move in [1, -1]:
+        for rotation in [0, -1, 1]:
+            A.append((move, rotation))
+    return A
 
 
 def P_function(s, a, s_):
-	pass
+    """
+    Returns probability of reaching state s_,  given current state, and an action 
+    Errors can happen if the robot pre-rotates left or right
+    """
+    # if a[0] == 0:
+    #     # If no movement occurs, the current state must equal the next state
+    #     if s == s_:
+    #         prob = 1.0
+    #     else:
+    #         prob = 0.0
+
+    # # Errors may happen if movement occurs
+    # else:
+        # New initial states with pre-rotation error
+    s_left = (s[0], s[1], s[2]-1) 
+    s_right = (s[0], s[1], s[2]+1)
+
+    # Compute all possible next states
+    s_next = next_state_no_error(s, a)
+    s_next_left = next_state_no_error(s_left, a)
+    s_next_right = next_state_no_error(s_right, a)
+
+    # Compare s_ with next states and increment probability
+    prob = 0.0
+    if s_ == s_next:
+        prob += (1 - 2*p_error)
+    if s_ == s_next_left:
+        prob += p_error
+    if s_ == s_next_right:
+        prob += p_error
+
+    return prob
 
 
 def R_function(s, a, s_):
-	pass
+    
+    s_x = s_[0]
+    s_y = s_[1]
 
-
-def move(s, a, L, W):
-	"""
-	Perform an action, without the pre-rotation error
-	Returns s_
-	"""
-
-	# check heading and increment position by +/- 1
-	if 2 <= s[2] <= 4:
-	    x = s[0] + a[0]
-	    y = s[1]
-
-	elif 5 <= s[2] <= 7:
-	    x = s[0]
-	    y = s[1] - a[0]
-
-	elif 8 <= s[2] <= 10:
-	    x = s[0] - a[0]
-	    y = s[1]
-
-	else: # 11, 0, 1
-	    x = s[0] 
-	    y = s[1] + a[0]
-
-	# boundary checks (can't fall off the grid)
-	if x < 0 or x >= L:
-	    x = s[0]
-	if y < 0 or y >= W:
-	    y= s[1]
-
-	# set the new heading (increment by +/- 1)
-	h = (s[2] + a[1]) % 12
-
-	s_ = (x, y, h)
-	return s_
-
-
-def p_sa(p_error, s, a, s_):
-	"""
-	Returns likeliihood of state s_ given the error prob, current state, and an action 
-	Errors can happen if the robot pre-rotates left or right
-	This function computes the next state in each case and compares it to s_
-
-	"""
-
-	if a[0] == 0:
-		# If no movement occurs, the current state must equal the next state
-	    if s == s_:
-	        prob = 1
-	    else:
-	        prob = 0
-
-	# Errors may happen if movement occurs
-	else:
-	    # New initial states with pre-rotation error
-	    s_left = (s[0], s[1], s[2]-1) 
-	    s_right = (s[0], s[1], s[2]+1)
-
-	    # Compute all possible next states
-	    s_next = move(s, a, L, W)
-	    s_next_left = move(s_left, a, L, W)
-	    s_next_right = move(s_right, a, L, W)
-
-	    # Compare s_ with next states and increment probability
-	    prob = 0
-	    if s_ == s_next:
-	        prob += (1 - 2*p_error)
-	    if s_ == s_next_left:
-	        prob += p_error
-	    if s_ == s_next_right:
-	        prob += p_error
-
-	return prob
-
-
-def next_state(p_error, s, a):
-	# Find all the next possible states, then determine which has highest probability
-	s_next = [
-		(s[0], s[1], s[2]),
-		(s[0], s[1], s[2]+a[1]),
-		(s[0]+a[0], s[1], s[2]),
-		(s[0]+a[0], s[1], s[2]+a[1]),
-		(s[0], s[1]+a[0], s[2]),
-		(s[0], s[1]+a[0], s[2]+a[1]),
-	]
-
-	max_p = 0
-	for state in s_next:
-		p = p_sa(p_error, s, a, state)
-		if p > max_p:
-			max_p = p
-			s_ = state
-
-	return s_
-
-
-gamma = 0.9
-
-L = 8
-W = 8
-
-# Testing p_sa and next_state functions here
-prob = p_sa(0, (0,0,0), (1,1), (0,1,1))
-print(prob)
-s = next_state(0.025, (7,7,11), (-1,0))
-print(s)
-
-states = get_states(L, W)
-actions = get_actions()
-
-mdp = MDP(states, actions, P_function, R_function, gamma)
-
-
-##########################
-#reward function R(s)
-
-def reward(s_x, s_y):
-    #out of the world
-    if ( s_x >= L or s_y >= W): 
+    if ( s_x >= L or s_y >= W or s_x<0 or s_y<0): 
         raise ValueError("Exceed World Size")
         
     #win!
-    if (s_x == 5 or s_y == 6):
+    if (s_x == 5 and s_y == 6):
         return 1
     
     #boundary 
@@ -160,21 +78,83 @@ def reward(s_x, s_y):
     return 0
 
 
-##########################
-#plotter
-#receives input as a list of all previous states i.e. [(x1,y1,h1), (x2,y2,h2)...]
-#plots location and orientation of the robot throughout the entire time history
+def next_state_no_error(s, a):
+    """
+    Perform an action, without the pre-rotation error
+    Returns s_
+    """
 
-#example input
-spot = np.array([(1,1,1),(1,2,2),(1,3,3),(2,3,5,),(2,4,10),(3,4,12),(3,5,4)])
+    # check heading and increment position by +/- 1
+    if 2 <= s[2] <= 4:
+        x = s[0] + a[0]
+        y = s[1]
+
+    elif 5 <= s[2] <= 7:
+        x = s[0]
+        y = s[1] - a[0]
+
+    elif 8 <= s[2] <= 10:
+        x = s[0] - a[0]
+        y = s[1]
+
+    else: # 11, 0, 1
+        x = s[0] 
+        y = s[1] + a[0]
+
+    # boundary checks (can't fall off the grid)
+    if x < 0 or x >= L:
+        x = s[0]
+    if y < 0 or y >= W:
+        y= s[1]
+
+    # set the new heading (increment by +/- 1)
+    h = (s[2] + a[1]) % 12
+
+    s_ = (x, y, h)
+    return s_
 
 
-L = 8 #y
-W = 8 #x
+# def all_next_states(s, a):
+#     # Find all the next possible states, then determine which has highest probability
+#     s_next = [
+#         (s[0], s[1], s[2]),
+#         (s[0], s[1], s[2]+a[1]),
+#         (s[0]+a[0], s[1], s[2]),
+#         (s[0]+a[0], s[1], s[2]+a[1]),
+#         (s[0], s[1]+a[0], s[2]),
+#         (s[0], s[1]+a[0], s[2]+a[1]),
+#     ]
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
+#     # max_p = 0
+#     # for state in s_next:
+#     #     p = P_function(s, a, state)
+#     #     if p > max_p:
+#     #         max_p = p
+#     #         s_ = state
+
+#     return s_next
+
+def next_state_draw(s, a):
+    # Find all the next possible states, then determine which has highest probability
+
+    weights = []
+    next_states = []
+
+    for s_ in states:
+        p = P_function(s, a, s_)
+        if p > 0:
+            next_states.append(s_)
+            weights.append(p)
+
+    # print(weights)
+    # print(next_states)
+    assert(sum(weights) == 1.0)
+
+    si_ = np.random.choice(range(len(next_states)), 1, weights)[0]
+    # print(si_)
+    # print(weights[si_])
+    
+    return next_states[si_]
 
 
 def plotter(spot):
@@ -185,7 +165,6 @@ def plotter(spot):
     for i in spot:
         spot_x_y += [(i[0]+0.5,i[1]+0.5)]
         ax.arrow(i[0]+0.5,i[1]+0.5, 0.5*np.sin(i[2]*np.pi/6),0.5*np.cos(i[2]*np.pi/6), head_width = 0.1, head_length = 0.2, fc = 'k', ec = 'k')
-
     
     spot_x_y = np.array(spot_x_y)
     
@@ -209,5 +188,95 @@ def plotter(spot):
     plt.grid()
     plt.savefig("fig.jpg", dpi = 300)
     plt.show()
+
+
+def inital_policy():
+    pi_0 = {}
+    tgt = (5, 6)
+
+    for s in states:
+
+        if s[0] == tgt[0] and s[1] == tgt[1]:
+            a0 = 0
+            a1 = 0
+
+        else:
+            tgt_direction = (tgt[0]-s[0], tgt[1]-s[1])
+            h_angle = np.deg2rad(30*s[2])
+            my_direction = (np.sin(h_angle), np.cos(h_angle))
+
+            if my_direction[0]*tgt_direction[0] + my_direction[1]*tgt_direction[1] >= 0:
+                a0 = +1
+            else:
+                a0 = -1
+
+            hr_angle = np.deg2rad(30*(s[2]+1))
+            hl_angle = np.deg2rad(30*(s[2]-1))
+
+            l_direction = (np.sin(hl_angle), np.cos(hl_angle))
+            r_direction = (np.sin(hr_angle), np.cos(hr_angle))
+
+            l_dotprod = l_direction[0]*tgt_direction[0] + l_direction[1]*tgt_direction[1]
+            r_dotprod = r_direction[0]*tgt_direction[0] + r_direction[1]*tgt_direction[1]
+
+            if l_dotprod > r_dotprod:
+                a1 = -1
+            elif l_dotprod < r_dotprod:
+                a1 = +1
+            else:
+                a1 = 0
+
+        pi_0[s] = (a0, a1)
+
+    return pi_0
+
+
+# globals
+gamma = 0.9
+L = 8
+W = 8
+p_error = 0.25
+
+if __name__ == "__main__":
+
+    # 1a
+    states = get_states()
+    print("Number of states:", len(states))
+    print()
+
+    # 1b
+    actions = get_actions()
+    print("Number of actions:", len(actions))
+    print()
+
+    # 1c
+    for i in range(5):
+        s = choice(states)
+        a = choice(actions)
+        s_ = next_state_draw(s, a)
+
+        print("P(",s,",",a,",",s_,"):", P_function(s, a, s_))
     
-plotter(spot)
+    print()
+
+    # 1d
+    for i in range(5):
+        s = choice(states)
+        a = choice(actions)
+
+        print("s =",s,", a =",a,": s_ =",s_, next_state_draw(s, a))
+
+    print()
+
+    # 2a
+    for i in range(5):
+        s_ = choice(states)
+        print("R(", s_, "):",R_function(None, None, s_))
+
+    print()
+
+    # mdp = MDP(states, actions, P_function, R_function, gamma)
+    # pi_star = mdp.value_iteration()
+
+    # spot = np.array([(1,1,1),(1,2,2),(1,3,3),(2,3,5,),(2,4,10),(3,4,12),(3,5,4)])
+    # plotter(spot)
--- mdp.py
+++ mdp.py
@@ -2,151 +2,155 @@ import numpy as np
 
 
 class MDP:
-	"""
-	Class for a general MDP
+    """
+    Class for a general MDP
 
-	Attributes:
-		states (list)
-		actions (list)
-		P_function (function(s,a,s_))
-		R_function (function(s,a,s_))
-		gamma (float)
+    Attributes:
+        states (list)
+        actions (list)
+        P_function (function(s,a,s_))
+        R_function (function(s,a,s_))
+        gamma (float)
 
-	"""
+    """
 
-	def __init__(self, states, actions, P_function, R_function, gamma):
+    def __init__(self, states, actions, P_function, R_function, gamma):
 
-		self.states = states
-		self.actions = actions
-		self.P_function = P_function
-		self.R_function = R_function
-		self.gamma = gamma
+        self.states = states
+        self.actions = actions
+        self.P_function = P_function
+        self.R_function = R_function
+        self.gamma = gamma
 
-		self.NS = len(states)
-		self.NA = len(actions)
-
-
-	def evaluate_policy(self, pi):
-		"""
-		Args:
-			pi (dict)
-
-		Returns:
-			V function in the form of a dict mapping each s to V(s),
-			Q function in the form of a dict mapping each (s,a) to Q(s,a)
-		"""
-		R_matrix = np.zeros((self.NS, self.NS))
-		P_matrix = np.zeros((self.NS, self.NS))
-
-		for i, s in enumerate(self.states):
-		
-			a = pi[s]
-		
-			for i_, s_ in enumerate(self.states):
-		
-				R_matrix[i,i_] = self.R_function(s, a, s_)
-				P_matrix[i,i_] = self.P_function(s, a, s_)
-
-		A = np.eye(self.NS)-self.gamma*P_matrix
-		b = (P_matrix*R_matrix).sum(axis=1)
-
-		V = dict(zip(self.states, np.linalg.solve(A,b)))
-
-		Q = {}
-
-		for s in self.states:
-
-			for a in self.actions:
-				
-				qval = 0
-				
-				for s_ in self.actions:
-
-					qval += self.P_function(s, a, s_)*(self.R_function(s, a, s_) + self.gamma*V[s_])
-
-				Q[(s,a)] = qval
-
-		# print(V, Q)
-
-		return V, Q
+        self.NS = len(states)
+        self.NA = len(actions)
 
 
-	def policy_iteration(self):
-		"""
-		Returns:
-			pi_star (dict)
-		"""
-		pi = {s:self.actions[0] for s in self.states}
-		V_star = {}
+    def evaluate_policy(self, pi):
+        """
+        Args:
+            pi (dict)
 
-		while True:
+        Returns:
+            V function in the form of a dict mapping each s to V(s),
+            Q function in the form of a dict mapping each (s,a) to Q(s,a)
+        """
+        R_matrix = np.zeros((self.NS, self.NS))
+        P_matrix = np.zeros((self.NS, self.NS))
 
-			V, Q = self.evaluate_policy(pi)
-			pi_ = pi.copy()
+        for i, s in enumerate(self.states):
+        
+            a = pi[s]
+        
+            for i_, s_ in enumerate(self.states):
+        
+                R_matrix[i,i_] = self.R_function(s, a, s_)
+                P_matrix[i,i_] = self.P_function(s, a, s_)
 
-			for s in self.states:
-				
-				for a in self.actions:
+        A = np.eye(self.NS)-self.gamma*P_matrix
+        b = (P_matrix*R_matrix).sum(axis=1)
 
-					if Q[(s,a)] > Q[(s,pi_[s])]:
+        V = dict(zip(self.states, np.linalg.solve(A,b)))
 
-						pi_[s] = a
+        Q = {}
 
-			if pi==pi_:
+        for s in self.states:
 
-				return V, pi
+            for a in self.actions:
+                
+                qval = 0
+                
+                for s_ in self.states:
 
-			pi = pi_
+                    qval += self.P_function(s, a, s_)*(self.R_function(s, a, s_) + self.gamma*V[s_])
 
+                Q[(s,a)] = qval
 
-	def value_iteration(self):
-		"""
-		Returns:
-			pi_star (dict)
-		"""
-		eps = 1e-6
-		R_3darray = np.zeros((self.NS,self.NA,self.NS))
-		P_3darray = np.zeros((self.NS,self.NA,self.NS))
+        # print(V, Q)
 
-		for si, s in enumerate(self.states):
-			
-			for ai, a in enumerate(self.actions):
-			
-				for si_, s_ in enumerate(self.states):
-
-					R_3darray[si,ai,si_] = self.R_function(s, a, s_)
-					P_3darray[si,ai,si_] = self.P_function(s, a, s_)
-
-		V = np.zeros(self.NS)
-
-		while True:
-
-			# print(V)
-
-			V_ = ((P_3darray*R_3darray).sum(axis=2) + self.gamma*(P_3darray.dot(V))).max(axis=1)
-
-			if np.abs(V_-V).max() < eps:
-				break
-
-			V = V_
+        return V, Q
 
 
-		# Q = {}
+    def policy_iteration(self):
+        """
+        Returns:
+            pi_star (dict)
+        """
+        pi = {s:self.actions[0] for s in self.states}
+        V_star = {}
 
-		# for si, s in enumerate(self.states):
+        # i = 0
 
-		# 	for ai, a in enumerate(self.actions):
-								
-		# 		Q[(s,a)] = P_3darray[si,ai,:].dot(R_3darray[si,ai,:]+self.gamma*V)
+        while True:
+            # i += 1
+            # print(i)
 
-		V_star = dict(zip(self.states, V))
-		
-		pi_star = ((P_3darray*R_3darray).sum(axis=2) + self.gamma*(P_3darray.dot(V))).argmax(axis=1)
-		pi_star = {self.states[i]:self.actions[pi_star[i]] for i in range(self.NS)}
+            V, Q = self.evaluate_policy(pi)
+            pi_ = pi.copy()
 
-		# print(pi_star)
-		
-		return V_star, pi_star
+            for s in self.states:
+                
+                for a in self.actions:
+
+                    if Q[(s,a)] > Q[(s,pi_[s])]:
+
+                        pi_[s] = a
+
+            if pi==pi_:
+
+                return V, pi
+
+            pi = pi_
+
+
+    def value_iteration(self):
+        """
+        Returns:
+            pi_star (dict)
+        """
+        eps = 1e-6
+        R_3darray = np.zeros((self.NS,self.NA,self.NS))
+        P_3darray = np.zeros((self.NS,self.NA,self.NS))
+
+        for si, s in enumerate(self.states):
+            
+            for ai, a in enumerate(self.actions):
+            
+                for si_, s_ in enumerate(self.states):
+
+                    R_3darray[si,ai,si_] = self.R_function(s, a, s_)
+                    P_3darray[si,ai,si_] = self.P_function(s, a, s_)
+
+        V = np.zeros(self.NS)
+
+        while True:
+
+            # print(V)
+
+            V_ = ((P_3darray*R_3darray).sum(axis=2) + self.gamma*(P_3darray.dot(V))).max(axis=1)
+
+            if np.abs(V_-V).max() < eps:
+                break
+
+            V = V_
+
+
+        # Q = {}
+
+        # for si, s in enumerate(self.states):
+
+        #   for ai, a in enumerate(self.actions):
+                                
+        #       Q[(s,a)] = P_3darray[si,ai,:].dot(R_3darray[si,ai,:]+self.gamma*V)
+
+        V_star = dict(zip(self.states, V))
+        
+        pi_star = ((P_3darray*R_3darray).sum(axis=2) + self.gamma*(P_3darray.dot(V))).argmax(axis=1)
+        pi_star = {self.states[i]:self.actions[pi_star[i]] for i in range(self.NS)}
+
+        # print(pi_star)
+        
+        return V_star, pi_star
 
 
 
@@ -155,24 +159,24 @@ class MDP:
 # test
 if __name__ == "__main__":
 
-	states = list(range(10))
-	actions = [0, 1]
+    states = list(range(10))
+    actions = [0, 1]
 
-	def P_function(s, a, s_):
-		n = len(states)
-		return 2*s_/(n*(n-1))
+    def P_function(s, a, s_):
+        n = len(states)
+        return 2*s_/(n*(n-1))
 
-	def R_function(s, a, s_):
+    def R_function(s, a, s_):
 
-		return s+a+s_
+        return s+a+s_
 
-	gamma = 0.9
+    gamma = 0.9
 
-	mdp = MDP(states, actions, P_function, R_function, gamma)
+    mdp = MDP(states, actions, P_function, R_function, gamma)
 
-	# mdp.evaluate_policy({0:0, 1:0})
+    # mdp.evaluate_policy({0:0, 1:0})
 
-	print(mdp.value_iteration())
+    print(mdp.value_iteration())
 
-	print(mdp.policy_iteration())
+    print(mdp.policy_iteration())
 
